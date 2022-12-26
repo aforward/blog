225b6c43ec2eb72263ec3c0c6b9b5f8a52913ea8
Andrew Forward <aforward@hey.com>
2022-12-26 13:20:12 -0500

    Rewrite hangman to use heex (over leex)

--- lib/blog_web/live/demo/hangman_live.ex
+++ lib/blog_web/live/demo/hangman_live.ex
@@ -20,96 +20,149 @@ defmodule BlogWeb.HangmanLive do
   @hit "#D64545"
 
   def render(assigns) do
-    ~L"""
+    ~H"""
     <style>
-    .hangman {
-    margin: 0 auto;
-    text-align: center;
-    }
+      .hangman {
+      margin: 0 auto;
+      text-align: center;
+      }
 
-    .hangman svg {
-    width: 500px;
-    height: auto;
-    }
+      .hangman svg {
+      width: 500px;
+      height: auto;
+      }
 
-    .hangman .word span {
-    font-size: 50px;
-    }
+      .hangman .word span {
+      font-size: 50px;
+      }
 
-    .hangman .word span + span {
-    padding-left: 20px;
-    }
+      .hangman .word span + span {
+      padding-left: 20px;
+      }
 
-    .hangman .form input[type='text'] {
-    width: 50px;
-    font-size: 50px;
-    height: 75px;
-    border: 1px solid #ABDB5E;
-    color: #94C843;
-    text-align: center;
-    }
+      .hangman .form input[type='text'] {
+      width: 50px;
+      font-size: 50px;
+      height: 75px;
+      border: 1px solid #ABDB5E;
+      color: #94C843;
+      text-align: center;
+      }
 
-    .hangman .form input[type='text'] {
-    font-size: 50px;
-    height: 75px;
-    }
+      .hangman .form input[type='text'] {
+      font-size: 50px;
+      height: 75px;
+      }
 
-    .hangman .move-reply {
-    font-size: 20px;
-    color: #A39E93;
-    }
+      .hangman .move-reply {
+      font-size: 20px;
+      color: #A39E93;
+      }
 
-    .hangman .move-reply span {
-    font-size: 25px;
-    color: #94C843;
-    }
+      .hangman .move-reply span {
+      font-size: 25px;
+      color: #94C843;
+      }
 
-    .hangman .btn-guess {
-    background-color: #ABDB5E;
-    padding: 0 10px 0 10px;
-    border-radius: 5px;
-    cursor: pointer;
-    }
+      .hangman .btn-guess {
+      background-color: #ABDB5E;
+      padding: 0 10px 0 10px;
+      border-radius: 5px;
+      cursor: pointer;
+      }
 
-    .hangman .btn-guess:hover {
-    background-color: #94C843;
-    color: #F2FDE0;
-    }
+      .hangman .btn-guess:hover {
+      background-color: #94C843;
+      color: #F2FDE0;
+      }
 
-    .hangman .btn-new {
-    background-color: #54D1DB;
-    color: #0E7C86;
-    padding: 0 10px 0 10px;
-    border-radius: 5px;
-    cursor: pointer;
-    }
+      .hangman .btn-new {
+      background-color: #54D1DB;
+      color: #0E7C86;
+      padding: 0 10px 0 10px;
+      border-radius: 5px;
+      cursor: pointer;
+      }
 
-    .hangman .btn-new:hover {
-    background-color: #0E7C86;
-    color: #54D1DB;
-    }
-
+      .hangman .btn-new:hover {
+      background-color: #0E7C86;
+      color: #54D1DB;
+      }
     </style>
 
-    <div class="hangman" data-cheater=<%= @answer %>>
+    <div class="hangman" data-cheater={assigns.answer}>
       <div class="move-reply">
-        <%= assigns |> top_message() |>  Phoenix.HTML.raw() %>
+        <%= assigns |> top_message() |> Phoenix.HTML.raw() %>
       </div>
-      <svg width="159px" height="144px" viewBox="0 0 159 144" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <svg
+        width="159px"
+        height="144px"
+        viewBox="0 0 159 144"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      >
         <g id="hangman" stroke="none" stroke-width="1" fill-rule="evenodd">
           <g id="platform" fill="#D1D1D1">
-            <polygon id="left" fill="<%= colour() %>" points="14 -7.66053887e-15 21 -7.66053887e-15 21 138 14 138"></polygon>
-            <polygon id="bottom" fill="<%= colour() %>" transform="translate(79.500000, 141.000000) rotate(90.000000) translate(-79.500000, -141.000000) " points="76.5 61.5 82.5 61.5 82.5 220.5 76.5 220.5"></polygon>
-            <polygon id="top" fill="<%= colour() %>" transform="translate(54.500000, 3.000000) rotate(90.000000) translate(-54.500000, -3.000000) " points="51.5 -51.5 57.5 -51.5 57.5 57.5 51.5 57.5"></polygon>
-            <polygon id="rope" fill="<%= person(@rope) %>" points="109 -1.88737914e-15 116 -1.88737914e-15 116 34 109 34"></polygon>
+            <polygon
+              id="left"
+              fill={colour()}
+              points="14 -7.66053887e-15 21 -7.66053887e-15 21 138 14 138"
+            >
+            </polygon>
+            <polygon
+              id="bottom"
+              fill={colour()}
+              transform="translate(79.500000, 141.000000) rotate(90.000000) translate(-79.500000, -141.000000) "
+              points="76.5 61.5 82.5 61.5 82.5 220.5 76.5 220.5"
+            >
+            </polygon>
+            <polygon
+              id="top"
+              fill={colour()}
+              transform="translate(54.500000, 3.000000) rotate(90.000000) translate(-54.500000, -3.000000) "
+              points="51.5 -51.5 57.5 -51.5 57.5 57.5 51.5 57.5"
+            >
+            </polygon>
+            <polygon
+              id="rope"
+              fill={person(assigns.rope)}
+              points="109 -1.88737914e-15 116 -1.88737914e-15 116 34 109 34"
+            >
+            </polygon>
           </g>
           <g id="person" transform="translate(83.000000, 34.000000)">
-            <ellipse id="head" fill="<%= person(@head) %>" cx="29.5" cy="12" rx="14.5" ry="12"></ellipse>
-            <rect id="body" fill="<%= person(@body) %>" x="26" y="23" width="7" height="36"></rect>
-            <polygon id="left_arm" fill="<%= person(@left_arm) %>" transform="translate(17.887481, 35.964726) rotate(100.000000) translate(-17.887481, -35.964726) " points="14.387481 21.4647257 21.387481 21.4647257 21.387481 50.4647257 14.387481 50.4647257"></polygon>
-            <polygon id="right_arm" fill="<%= person(@right_arm) %>" transform="translate(41.887481, 35.964726) rotate(80.000000) translate(-41.887481, -35.964726) " points="38.387481 21.4647257 45.387481 21.4647257 45.387481 50.4647257 38.387481 50.4647257"></polygon>
-            <polygon id="left_leg" fill="<%= person(@left_leg) %>" transform="translate(16.449946, 69.449946) rotate(45.000000) translate(-16.449946, -69.449946) " points="13.9041271 50.860171 20.7936782 50.5299419 18.9957643 88.0397203 12.1062131 88.3699494"></polygon>
-            <polygon id="right_leg" fill="<%= person(@right_leg) %>" transform="translate(42.449946, 71.449946) rotate(-45.000000) translate(-42.449946, -71.449946) " points="38.1062131 52.5299419 44.9957643 52.860171 46.7936782 90.3699494 39.9041271 90.0397203"></polygon>
+            <ellipse id="head" fill={person(assigns.head)} cx="29.5" cy="12" rx="14.5" ry="12">
+            </ellipse>
+            <rect id="body" fill={person(assigns.body)} x="26" y="23" width="7" height="36"></rect>
+            <polygon
+              id="left_arm"
+              fill={person(assigns.left_arm)}
+              transform="translate(17.887481, 35.964726) rotate(100.000000) translate(-17.887481, -35.964726) "
+              points="14.387481 21.4647257 21.387481 21.4647257 21.387481 50.4647257 14.387481 50.4647257"
+            >
+            </polygon>
+            <polygon
+              id="right_arm"
+              fill={person(assigns.right_arm)}
+              transform="translate(41.887481, 35.964726) rotate(80.000000) translate(-41.887481, -35.964726) "
+              points="38.387481 21.4647257 45.387481 21.4647257 45.387481 50.4647257 38.387481 50.4647257"
+            >
+            </polygon>
+            <polygon
+              id="left_leg"
+              fill={person(assigns.left_leg)}
+              transform="translate(16.449946, 69.449946) rotate(45.000000) translate(-16.449946, -69.449946) "
+              points="13.9041271 50.860171 20.7936782 50.5299419 18.9957643 88.0397203 12.1062131 88.3699494"
+            >
+            </polygon>
+            <polygon
+              id="right_leg"
+              fill={person(assigns.right_leg)}
+              transform="translate(42.449946, 71.449946) rotate(-45.000000) translate(-42.449946, -71.449946) "
+              points="38.1062131 52.5299419 44.9957643 52.860171 46.7936782 90.3699494 39.9041271 90.0397203"
+            >
+            </polygon>
           </g>
         </g>
       </svg>
