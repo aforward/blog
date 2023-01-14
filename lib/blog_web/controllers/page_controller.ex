3868605e42a1752e21cb5d0f37b9405d28c4029c
Andrew Forward <aforward@hey.com>
2023-01-14 14:25:18 -0500

    Add meta description that is page specific

--- lib/blog_web/controllers/page_controller.ex
+++ lib/blog_web/controllers/page_controller.ex
@@ -9,17 +9,32 @@ defmodule BlogWeb.PageController do
 
   def books(conn, _params) do
     conn
+    |> assign(:page_title, "Book, Books, Books")
+    |> assign(
+      :meta_description,
+      "A mix of tech, fiction, business and life... sometimes I read, sometimes I am read to."
+    )
     |> assign(:books, Gen.Books.books())
     |> render(:books)
   end
 
   def demos(conn, _params) do
     conn
+    |> assign(:page_title, "Technical Demos")
+    |> assign(
+      :meta_description,
+      "A collection of demos that have been built to support a talk, to learn a concept, or maybe just for fun."
+    )
     |> render(:demos)
   end
 
   def snippets(conn, _params) do
     conn
+    |> assign(:page_title, "Technical Tidbits")
+    |> assign(
+      :meta_description,
+      "A collection of code snippets of sometimes rarely used, or perhaps difficult to remember tasks, commands and code snippets."
+    )
     |> assign(:snippets, Gen.Snippets.snippets())
     |> render(:snippets)
   end
@@ -28,6 +43,11 @@ defmodule BlogWeb.PageController do
     meta = Gen.Articles.meta(slug)
 
     conn
+    |> assign(:page_title, meta[:title])
+    |> assign(
+      :meta_description,
+      "More details about '#{meta[:title]}' written (last updated) on #{meta[:datetime]}."
+    )
     |> assign(:slug, slug)
     |> assign(:title, meta[:title])
     |> assign(:datetime, meta[:datetime])
